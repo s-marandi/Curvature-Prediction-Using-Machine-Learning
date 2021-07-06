d61fa85a3d597742ae5c5778e4ad74d5564a774a
s-marandi <87037027+s-marandi@users.noreply.github.com>
2021-07-06 15:55:53 -0400

    Update Combined.py

--- Combined.py
+++ Combined.py
@@ -63,7 +63,7 @@ def main():
             super(Net, self).__init__()
             self.fc1 = nn.Linear(9 * 1, 100)
             self.fc2 = nn.Linear(200, 200)
-            self.fc3 = nn.Linear(200, 10)
+            self.fc3 = nn.Linear(100, 1)
 ​
         def forward(self, x):
             x = F.relu(self.fc1(x))
