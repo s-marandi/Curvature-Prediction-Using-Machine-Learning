2c64031a95fb8e2538dc4b958a0c215a7bf5b982
s-marandi <87037027+s-marandi@users.noreply.github.com>
2021-07-06 14:36:38 -0400

    Add files via upload

---
+++ loadDS_vf.py
@@ -0,0 +1,46 @@
+import numpy as np
+import torch as T
+
+device = T.device("cpu")
+
+class VolFracDataset(T.utils.data.Dataset):
+    def __init__(self, src_file):
+        x_tmp = np.loadtxt(src_file, usecols=range(1,10))
+        y_tmp = np.loadtxt(src_file, usecols=0)
+
+        self.x_data = T.tensor(x_tmp).to(device)
+        self.y_data = T.tensor(y_tmp).to(device)
+
+    def __len__(self):
+        return len(self.x_data)
+
+    def __getitem__(self, idx):
+        if T.is_tensor(idx):
+          idx = idx.tolist()
+        inp = self.x_data[idx, :]
+        out = self.y_data[idx]
+        sample = \
+          { 'f' : inp, 'hk' : out }
+        return sample
+
+
+def main():
+    train_file = "learnClean.txt"
+    # Instantiate the class
+    train_ds = VolFracDataset(train_file)
+    train_ldr = T.utils.data.DataLoader(train_ds, batch_size=3, shuffle=True)
+
+    for epoch in range(2):
+        print("\n==============================\n")
+        print("Epoch = " + str(epoch))
+        for (batch_idx, batch) in enumerate(train_ldr):
+            print("\nBatch = " + str(batch_idx))
+            X = batch['f']
+            Y = batch['hk']
+            print(batch_idx)
+            print(Y)
+
+
+main()
+#if __name__ == "__main__":
+#    main()
